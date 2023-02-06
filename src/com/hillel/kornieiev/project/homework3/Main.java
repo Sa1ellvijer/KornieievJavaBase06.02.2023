720fe30318cb7c8760a6abcf0f79510ab1c885a9
Artur Kornieiev <Saiellvijer@gmail.com>
2023-02-06 21:32:58 +0200

    Homework3 and ignore

--- src/com/hillel/kornieiev/project/homework3/Main.java
+++ src/com/hillel/kornieiev/project/homework3/Main.java
@@ -4,5 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
+        byte SideLength = 10;
+        byte SideWidth = 5;
+        byte SideHeight = 5;
+
+        int volume = SideLength*SideWidth*SideHeight;
+        int lenght = SideLength*4;
+
+        System.out.println();
+        System.out.println("Об'єм паралелепіпеда = " + volume);
+        System.out.println("Сума довжин паралелепіпеда = " + lenght);
     }
 }
