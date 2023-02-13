964837bc8e4aaa612a19666e20f31e26404bb5d6
Sa1ellvijer <Saiellvijer@gmail.com>
2023-02-13 20:27:26 +0200

    homework5

--- src/com/hillel/kornieiev/project/homework5/Main.java
+++ src/com/hillel/kornieiev/project/homework5/Main.java
@@ -1,5 +1,28 @@
-package com.hillel.kornieiev.project.homework4;
+package com.hillel.kornieiev.project.homework5;
 
 public class Main {
-    
+
+    public static void main(String[] args) {
+
+        // Dynasty Li
+
+        int WarriorLi = 13;
+        int ArcherLi = 24;
+        int HorsemanLi = 46;
+        int TotalPeopleLi = 860;
+
+        // Dynasty Min
+
+        int WarriorMin = 9;
+        int ArcherMin = 35;
+        int HorsemanMin = 12;
+        double TotalPeopleMin = 860*1.5;
+
+        System.out.println();
+        System.out.println("Династія Лі, загальний показник атаки:");
+        System.out.println((WarriorLi+ArcherLi+HorsemanLi)*TotalPeopleLi);
+        System.out.println();
+        System.out.println("Династія Мінь, загальний показник атаки:");
+        System.out.println((WarriorMin + ArcherMin + HorsemanMin)*TotalPeopleMin);
+    }
 }
