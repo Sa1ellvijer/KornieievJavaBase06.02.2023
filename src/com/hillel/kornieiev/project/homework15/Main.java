ebb28de14dfea64906c243423c60bfa15e1a3c55
Sa1ellvijer <Saiellvijer@gmail.com>
2023-03-15 20:22:40 +0200

    Homework 15. v.2

--- src/com/hillel/kornieiev/project/homework15/Main.java
+++ src/com/hillel/kornieiev/project/homework15/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println();
         MusicStyles[] music = {
                 new ClassicMusic("Mozart"),
                 new PopMusic("Sia"),
